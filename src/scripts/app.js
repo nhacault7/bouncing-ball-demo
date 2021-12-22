83326a336569f81a7baf2d837f26733580b6de59
Nicholas Hacault <nicholashacaulttabs@gmail.com>
2021-12-22 14:54:52 -0600

    add draw method to Ball class

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -15,11 +15,20 @@ function random(min, max) {
 
 class Ball {
   constructor(x, y, velX, velY, color, size) {
-    this.x = x;
-    this.y = y;
-    this.velX = velX;
-    this.velY = velY;
-    this.color = color;
-    this.size = size;
+    this.x = x;             // Starting horizontal position
+    this.y = y;             // Starting vertical position
+    this.velX = velX;       // Horizontal velocity
+    this.velY = velY;       // Vertical velocity
+    this.color = color;     // Ball color
+    this.size = size;       // Ball size
+  }
+
+  draw() {
+    ctx.beginPath();                              // State we want to "draw" on canvas
+    ctx.fillStyle = this.color;                   // Define the color of the shape
+    ctx.arc(                                      // Draw circle
+      this.x, this.y, this.size, 0, 2 * Math.PI   // 0 = starting degree, 2 * PI = 360 (radians)
+      );                                          
+    ctx.fill();                                   // State we want to finish "drawing"
   }
 }
