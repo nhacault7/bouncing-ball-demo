f0ac78a733b2cfdaf25059a8c44b46987860960d
Nicholas Hacault <nicholashacaulttabs@gmail.com>
2021-12-22 15:25:48 -0600

    add loop to fill array with Ball objects

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -59,3 +59,21 @@ class Ball {
     this.y += this.velY;
   }
 }
+
+let balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),            
+    random(0 + size, height - size),           
+    random(-7, 7),                                                
+    random(-7, 7),                                                
+    `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`, 
+    size,                                               
+  );
+
+  balls.push(ball);
+}
